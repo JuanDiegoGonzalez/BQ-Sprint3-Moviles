5d0b20375cd843b832d2239caa8d2a2b3b0b0988
JuanDiegoGonzalez <jd.gonzalezg1@uniandes.edu.co>
2022-05-25 02:12:44 -0500

    Answer highlighting

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const names = ["Football", "Basketball", "Tennis", "Volleyball", "Gym"]
 
 function rendersports(doc){
     let values = [doc.data().football, doc.data().basketball, doc.data().tennis, doc.data().volleyball, doc.data().gym]
+    var ul = document.getElementById("sports-list");
 
     if (sportsList.childNodes.length == 0) {
         for (i=0; i<5; i++)
@@ -20,20 +21,21 @@ function rendersports(doc){
             li.appendChild(value);
 
             sportsList.appendChild(li);
+            ul.getElementsByTagName("LI").item(i).id = "li" + i
         }
     }
     else {
         for (i=0; i<5; i++)
         {
             document.getElementById('value' + names[i]).innerHTML = values[i]
+            ul.getElementsByTagName("LI").item(i).id = "li" + i
         }
     }
 
-    var ul = document.getElementById("sports-list");
-    console.log(ul.getElementsByTagName("LI").item(0).childNodes.item(1).textContent)
     Array.from(ul.getElementsByTagName("LI"))
       .sort((b, a) => a.childNodes.item(1).textContent.localeCompare(b.childNodes.item(1).textContent, 'en', {numeric: true}))
       .forEach(li => ul.appendChild(li));
+    ul.getElementsByTagName("LI").item(0).id = "primero"
 }
 
 db.collection("sportsUsersCount")
